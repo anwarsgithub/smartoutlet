59cf0fa01d57faa5f763a697acc8375ad2637874
Leonardo Gutierrez <lguti068@fiu.edu>
2019-02-04 15:52:56 -0500

    Added login and register support

--- website/smart_outlet_site/main_site/urls.py
+++ website/smart_outlet_site/main_site/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.home, name='main-home'),
-    path('about/', views.about, name="main-about")
+    path('signup/', views.signup, name='main-signup'),
+    path('about/', views.about, name='main-about'),
+    path('accounts/', include('django.contrib.auth.urls'), name='main-login'),
 ]
--- website/smart_outlet_site/main_site/views.py
+++ website/smart_outlet_site/main_site/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'main_site\home.html')
+    return render(request, 'main_site/home.html')
 
 def about(request):
-    return render(request, 'main_site\about.html')
+    return render(request, 'main_site/about.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'main_site/signup.html', {'form': form})
